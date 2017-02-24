e50f73bff6fbfac49dfee70e8c5e5d195a30e2e3
fteppe <teppe.florent@hotmail.fr>
2017-02-24 06:52:53 -0500

    petit changement dans les etats implémentés

--- EtatImpl.h
+++ EtatImpl.h
@@ -3,8 +3,8 @@
 
 #include "Etat.h"
 
-public Symbole;
-public Automate;
+#include "Symbole.h"
+#include "Automate.h"
 
 class E0 : public Etat{
 
@@ -20,52 +20,61 @@ class E1 : public Etat{
 	public:
 		inline E1(Symbole s):Etat(s){}
 		virtual ~Etat();
+		virtual void transition(Automate &automate, Symbole * symbole);
 };
 class E2 : public Etat{
 	public:
 		inline E2(Symbole s):Etat(s){}
 		virtual ~Etat();
+		virtual void transition(Automate &automate, Symbole * symbole);
 };
 
 class E3 : public Etat{
 	public:
 		inline E3(Symbole s):Etat(s){}
 		virtual ~Etat();
+		virtual void transition(Automate &automate, Symbole * symbole);
 };
 
 class E4 : public Etat{
 	public:
 		inline E4(Symbole s):Etat(s){}
 		virtual ~Etat();
+		virtual void transition(Automate &automate, Symbole * symbole);
 };
 
 class E5 : public Etat{
 	public:
 		inline E5(Symbole s):Etat(s){}
 		virtual ~Etat();
+		virtual void transition(Automate &automate, Symbole * symbole);
 };
 
 class E6 : public Etat{
 	public:
 		inline E6(Symbole s):Etat(s){}
 		virtual ~Etat();
+		virtual void transition(Automate &automate, Symbole * symbole);
 };
 
 class E7 : public Etat{
 	public:
 		inline E7(Symbole s):Etat(s){}
 		virtual ~Etat();
+		virtual void transition(Automate &automate, Symbole * symbole);
 };
 
 class E8 : public Etat{
 	public:
 		inline E8(Symbole s):Etat(s){}
 		virtual ~Etat();
+		virtual void transition(Automate &automate, Symbole * symbole);
 };
 
 class E9 : public Etat{
 	public:
 		inline E9(Symbole s):Etat(s){}
 		virtual ~Etat();
+		virtual void transition(Automate &automate, Symbole * symbole);
 };
 #endif //ETATIMPL_H
